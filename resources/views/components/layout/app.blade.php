87b7f72bd5b6b50ac963b7afc9c3856a0d8c16e7
Muhammad Akbar Reishandy <akbar@reishandy.my.id>
2025-06-11 15:28:00 +0700

    Login and Register view

---
+++ resources/views/components/layout/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap"
+          rel="stylesheet">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="flex items-center justify-center min-h-screen bg-base-300">
+{{ $slot }}
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// TODO: Remove
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/dummy', function () {
+    return view('welcome');
+})->name('password.request');
+
+
+require __DIR__.'/auth.php';
+
+// TODO list:
+//  - custom error
+//  - custom pagination

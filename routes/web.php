ab5e10df475c1dbaa427b46bb9d9dc2f027486fc
Muhammad Akbar Reishandy <akbar@reishandy.my.id>
2025-06-14 07:53:11 +0700

    Finalizing

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 })->name('home');
 
 Route::controller(KanbanController::class)->group(function () {
